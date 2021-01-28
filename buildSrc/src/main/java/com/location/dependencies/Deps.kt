c39ee079bb5c35c1e13768dae25e918c23adc92e
tianxiaolong <tianxiaolong@huami.com>
2021-01-28 23:41:36 +0800

    ref |> 使用buildSrc替代version.gradle

---
+++ buildSrc/src/main/java/com/location/dependencies/Deps.kt
@@ -0,0 +1,38 @@
+package com.location.dependencies
+
+
+/**
+ *
+ * @author tianxiaolong
+ * time：2021/1/28 11:00 PM
+ * description：
+ */
+
+object AppConfig{
+    const val APPLICATION_ID = "com.location.wanandroid"
+    const val compile_version = 30
+    const val build_tool_version = "30.0.3"
+    const val min_sdk_version = 27
+    const val target_sdk_version = 30
+    const val IS_BINDING = true
+    const val version_code = 1
+    const val version_name = "1.0.0"
+
+}
+object  Versions{
+    const val kotlin = "1.3.72"
+    const val core_ktx_version = "1.3.2"
+    const val appcompat_version = "1.2.0"
+    const val material_version = "1.2.1"
+    const val junit_vrsion = "4.13.1"
+    const val espresso_core_version = "3.3.0"
+    const val lifecycle_version = "2.2.0"
+    const val startup_version = "1.0.0"
+    const val fragment_version = "1.2.5"
+    const val navigation_version = "2.3.2"
+    const val coroutines_version = "1.3.0"
+}
+
+object Deps {
+
+}
