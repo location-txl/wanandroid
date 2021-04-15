4d46b5852be88311a5bc077a67146e5b1746c267
tianxiaolong <tttx0307@163.com>
2021-04-15 23:30:32 +0800

    ref |> BaseViewHolder 增加工厂类方法

--- base/src/main/java/com/location/base/BaseViewHolder.kt
+++ base/src/main/java/com/location/base/BaseViewHolder.kt
@@ -11,4 +11,25 @@ import androidx.recyclerview.widget.RecyclerView
  */
 abstract class BaseViewHolder<V:ViewDataBinding,T>(val binding:V) :RecyclerView.ViewHolder(binding.root){
     abstract fun onBind(data:T)
+
+
+    interface Factory{
+        fun <VH:BaseViewHolder<*,*>> create(clazz: Class<VH>,binding: ViewDataBinding):VH?
+    }
+
+    class DefaultFactory : Factory {
+
+
+        override fun <VH : BaseViewHolder<*, *>> create(clazz: Class<VH>, binding: ViewDataBinding): VH? {
+            try {
+                return clazz.getConstructor(binding::class.java.superclass).newInstance(binding)
+            } catch (e: NoSuchMethodException) {
+                e.printStackTrace()
+            }
+            return null
+        }
+
+
+    }
+
 }
