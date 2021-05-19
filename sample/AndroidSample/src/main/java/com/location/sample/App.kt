7da6059a26f9b5ebe3664673072ce0a25ae69c30
tianxiaolong <tttx0307@163.com>
2021-05-19 23:03:28 +0800

    feat |> startup初始化ARoute

--- sample/AndroidSample/src/main/java/com/location/sample/App.kt
+++ sample/AndroidSample/src/main/java/com/location/sample/App.kt
@@ -1,7 +1,6 @@
 package com.location.sample
 
 import android.app.Application
-import com.alibaba.android.arouter.launcher.ARouter
 
 /**
  *
@@ -10,12 +9,4 @@ import com.alibaba.android.arouter.launcher.ARouter
  * description：
  */
 class App: Application() {
-    override fun onCreate() {
-        super.onCreate()
-        if (BuildConfig.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
-            ARouter.openLog()     // 打印日志
-            ARouter.openDebug()   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
-        }
-        ARouter.init(this)
-    }
 }
---
+++ sample/AndroidSample/src/main/java/com/location/sample/startup/ARouteInit.kt
@@ -0,0 +1,28 @@
+package com.location.sample.startup
+
+import android.app.Application
+import android.content.Context
+import androidx.startup.Initializer
+import com.alibaba.android.arouter.launcher.ARouter
+import com.location.sample.BuildConfig
+
+/**
+ *
+ * @author tianxiaolong
+ * time：5/19/21 10:58 PM
+ * description：
+ */
+class ARouteInit: Initializer<Boolean> {
+    override fun create(context: Context): Boolean {
+        if (BuildConfig.DEBUG) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
+            ARouter.openLog()     // 打印日志
+            ARouter.openDebug()   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
+        }
+        ARouter.init(context as Application)
+        return true
+    }
+
+    override fun dependencies(): MutableList<Class<out Initializer<*>>> {
+        return mutableListOf()
+    }
+}
